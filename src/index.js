49b7476f843750e08e7a61e70e02be60c74838d6
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-03 16:58:30 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import  ReactDOM  from "react";
+import App from "./App";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>, document.querySelector('#root'));
+
