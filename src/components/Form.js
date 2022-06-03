bbf988873e53985cc6f11c4ea8c6ee67a16166fd
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-03 21:30:20 +0200

    create form.js

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Form() {
+  return (
+    <div>
+      <input type="text" placeholder="book name" />
+      <input type="text" placeholder="auhtor name" />
+      <button type="button">Add Book</button>
+    </div>
+  );
+}
+
+export default Form;
