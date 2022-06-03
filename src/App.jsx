1ed268aadd1f853b183c73c689135aad1461985b
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-03 21:22:38 +0200

    update app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Books from "./components/Books";
+import Categories from "./components/Categories";
+
+function App() {
+  return (
+    <>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="Categories">Categories</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="Categories" element={<Categories />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
