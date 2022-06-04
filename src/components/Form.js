5baf8ff0c656a1e7b5811daa73efd1a26a1652c0
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-04 10:07:33 +0200

    replace div tag  with form tag

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Form() {
   return (
-    <div>
+    <form>
       <input type="text" placeholder="book name" />
       <input type="text" placeholder="auhtor name" />
       <button type="button">Add Book</button>
-    </div>
+    </form>
   );
 }
 
