da78e380406561e51afdb07192a41e6416e46065
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-08 20:21:03 +0200

    add reducers and actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import Books from "./components/Books";
-import Categories from "./components/Categories";
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import Books from './components/Books';
+import Categories from './components/Categories';
 
 function App() {
   return (
--- src/components/Books.js
+++ src/components/Books.js
@@ -3,15 +3,22 @@ import Book from './Book';
 import Form from './Form';
 
 const Books = () => {
-  const name = 'GEDEON';
-  const author = 'USHINDI';
+  const bookArray = [
+    { name: 'GEDEON', author: 'USHINDI' },
+    { name: 'ADRIEL', author: 'TSHOBOHWA' },
+  ];
+
   return (
     <section>
       this will display a list of books!
-      <div>
-        <Book n={name} a={author} />
-        <button type="button">Delete all</button>
-      </div>
+      {
+        bookArray.map((x) => (
+          <div>
+            <Book n={x.name} a={x.author} />
+          </div>
+        ))
+      }
+      <button type="button">Delete all</button>
       here is the form generating the books!
       <Form />
     </section>
