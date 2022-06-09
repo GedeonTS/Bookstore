51dead6b44bf328cac0e58ebc88b97320ca31155
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-09 08:07:51 +0200

    connect to the redux store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './App.css';
+import store from './redux/configureStore';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>, document.querySelector('#root'),
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>, document.querySelector('#root'),
 );
