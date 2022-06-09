878235696daa6cd37830c495e4e70c255659daf5
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-09 13:03:29 +0200

    create checkstatus button's functionality

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
 function Categories() {
+  const dispatch = useDispatch();
+  const message = useSelector((state) => state.categories);
+
   return (
-    <div><button type="button">check Status</button></div>
+    <>
+      <h1>{message}</h1>
+      <div>
+        <button type="button" onClick={() => dispatch(checkStatus())}>check Status</button>
+      </div>
+    </>
   );
 }
 
