5caf8dd456ad9258ccca98247465f0f1c43142ab
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-09 13:02:15 +0200

    add removeBook event by creating action with dispatch

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
 function Book(props) {
-  const { n, a } = props;
+  const { n, a, id } = props;
+  const dispatch = useDispatch();
   return (
     <div>
       <div>
-        {n}{"  "}
-        by{"  "}
+        {n}
+        {' '}
+        by
+        {' '}
         {a}
       </div>
-      <button type="button">Delete</button>
+      <button type="button" onClick={() => dispatch(removeBook(id))}>Delete</button>
     </div>
   );
 }
