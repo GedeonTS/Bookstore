e2a0d5be449d7d93de924f74d5955783a96327e1
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-11 00:11:00 +0200

    import styles from app.css

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,17 @@ import Categories from './components/Categories';
 
 function App() {
   return (
-    <>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="Categories">Categories</Link>
+    <section className="main-container">
+      <nav className="navbar">
+        <h1>Bookstore CMS</h1>
+        <Link to="/" className="home-menu">BOOKS</Link>
+        <Link to="Categories" className="categories-menu">CATEGORIES</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Books />} />
         <Route path="Categories" element={<Categories />} />
       </Routes>
-    </>
+    </section>
   );
 }
 
