3eeb81d3e35902e1e3573351883c570d18087053
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-11 00:11:34 +0200

    add proper classes to componnents

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,16 +6,25 @@ function Book(props) {
   const [id, [bookItem]] = props.book;
   const dispatch = useDispatch();
   return (
-    <div>
-      <div>
-        <h2>{bookItem.title}</h2>
-        {' '}
-        by
-        {' '}
-        <h5>{bookItem.author}</h5>
+    <article className="book-item">
+      <div className="book-details">
+        <p className="book-category">{bookItem.category}</p>
+        <p className="book-title">{bookItem.title}</p>
+        <p className="book-author">{bookItem.author}</p>
+        <ul>
+          <li>
+            <button type="button">Comments</button>
+          </li>
+          <li>
+            <button type="button" id="btn-remove" onClick={() => dispatch(removeBook(id))}>Delete</button>
+          </li>
+          <li>
+            <button type="button">Edit</button>
+          </li>
+        </ul>
       </div>
-      <button type="button" onClick={() => dispatch(removeBook(id))}>Delete</button>
-    </div>
+
+    </article>
   );
 }
 
