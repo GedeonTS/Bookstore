f9680d70d897f7a5e50ab07211e0c658f6e0b737
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-11 11:47:38 +0200

    add contact icon

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Books from './components/Books';
 import Categories from './components/Categories';
+import contactImage from './images/contact.png';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <h1>Bookstore CMS</h1>
         <Link to="/" className="home-menu">BOOKS</Link>
         <Link to="Categories" className="categories-menu">CATEGORIES</Link>
+        <img className="image-contact" src={contactImage} alt="contact" />
       </nav>
       <Routes>
         <Route path="/" element={<Books />} />
